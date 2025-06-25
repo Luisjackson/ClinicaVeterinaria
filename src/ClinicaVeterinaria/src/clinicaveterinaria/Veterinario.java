6c1c59629c26c7e92b3464e89df19aa885e65292
Luis Jackson <151092088+Luisjackson@users.noreply.github.com>
2025-06-25 00:01:31 -0300

    Update Veterinario.java

--- src/ClinicaVeterinaria/src/clinicaveterinaria/Veterinario.java
+++ src/ClinicaVeterinaria/src/clinicaveterinaria/Veterinario.java
@@ -1,13 +1,14 @@
 public class Veterinario extends Funcionario{
-    private String especialidade, cfmv;
+    private String cfmv;
+    private Especialidade especialidade;
 
-    public Veterinario(String nome, String cpf, String email, String telefone, String turno, String especialidade, String cfmv){
-        super(nome, cpf, email, telefone, turno);
+    public Veterinario(String nome, String cpf, String email, String endereco, String telefone, String turno, Especialidade especialidade, String cfmv){
+        super(nome, cpf, email, endereco, telefone, turno);
         this.especialidade = especialidade;
         this.cfmv = cfmv;
     }
 
-    public void setEspecialidade(String especialidade){
+    public void setEspecialidade(Especialidade especialidade){
         this.especialidade = especialidade;
     }
 
@@ -15,12 +16,23 @@ public class Veterinario extends Funcionario{
         this.cfmv = cfmv;
     }
 
-    public String getEspecialiade(){
+    public Especialidade getEspecialiade(){
         return this.especialidade;
     }
 
     public String getCfmv(){
         return this.cfmv;
+    }
+
+    @Override
+    public String toString(){
+        return ("Nome:"+getNome() +
+                "\nCpf:"+getCpf()+
+                "\nEmail:"+getEmail()+
+                "\nEndereco:"+getEndereco()+
+                "\nTelefone:"+getTelefone()+
+                "\nTurno:"+getTurno()+
+                "\nEspecialidade:"+getEspecialiade());
     }
 
 }
