3be1bb3113485dbf0de21989221c672c797866cd
Luis Jackson <151092088+Luisjackson@users.noreply.github.com>
2025-06-27 00:10:02 -0300

    Update Agenda.java

--- src/ClinicaVeterinaria/src/clinicaveterinaria/Agenda.java
+++ src/ClinicaVeterinaria/src/clinicaveterinaria/Agenda.java
@@ -1,10 +1,11 @@
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class Agenda{
-    private LocalDate data, horario;
+public class Agenda {
+    private LocalDate data;
+    private LocalTime horario;
 
-    public Agenda(LocalDate data, LocalDate horario) {
+    public Agenda(LocalDate data, LocalTime horario) {
         this.data = data;
         this.horario = horario;
     }
@@ -13,23 +14,21 @@ public class Agenda{
         return data;
     }
 
-    public void setData(LocalDate data) {
-        this.data = data;
-    }
-
-    public LocalDate getHorario() {
+    public LocalTime getHorario() {
         return horario;
     }
 
-    public void setHorario(LocalDate horario) {
-        this.horario = horario;
+    public boolean isHorarioPermitido() {
+        int hora = horario.getHour();
+        int minuto = horario.getMinute();
+
+        //Se estiver dentro do horario permitido retorna true, se não retorna falso
+        return ((hora >= 8 && hora < 12) || (hora >= 14 && hora < 18)) &&
+                (minuto == 0 || minuto == 20 || minuto == 40);
     }
 
     @Override
     public String toString() {
-        return "Agenda{" +
-                "data=" + data +
-                ", horario=" + horario +
-                '}';
+        return data.toString() + " às " + horario.toString();
     }
 }
