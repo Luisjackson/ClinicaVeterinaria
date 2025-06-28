5bafa25aee91c392b0c59756a8016d67983d1f7a
Luis Jackson <luisjacksonjr@gmail.com>
2025-06-28 13:09:38 -0300

    ?

--- src/Clinica.java
+++ src/Clinica.java
@@ -1,63 +1,60 @@
 import java.util.ArrayList;
 
-public class Clinica
-{
-private String nome;
-private ArrayList<Animal> pacientes;
-private ArrayList<Funcionario> funcionarios;
-private ArrayList<Agenda> agendas;
-private ArrayList<Consulta> consultas;
+public class Clinica{
+    private String nome;
+    private ArrayList<Animal> pacientes;
+    private ArrayList<Funcionario> funcionarios;
+    private ArrayList<Agenda> agendas;
+    private ArrayList<Consulta> consultas;
     
     public Clinica (String nome){
-    this.nome = nome;
-    this.pacientes = new ArrayList<Animal>();
-    this.funcionarios = new ArrayList<Funcionario>();
-    this.agendas = new ArrayList<Agenda>();
-    this.consultas = new ArrayList<Consulta>();
+        this.nome = nome;
+        this.pacientes = new ArrayList<Animal>();
+        this.funcionarios = new ArrayList<Funcionario>();
+        this.agendas = new ArrayList<Agenda>();
+        this.consultas = new ArrayList<Consulta>();
     }
 
-// set
     
-public void setNome(String nome){
-    this.nome = nome;
-}
     
-public void addPaciente(Animal animal){
-    this.pacientes.add(animal);
-}
-    
-public void addFuncionario(Funcionario funcionario){
-    this.funcionarios.add(funcionario);
-}
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+        
+    public void addPaciente(Animal animal){
+        this.pacientes.add(animal);
+    }
+        
+    public void addFuncionario(Funcionario funcionario){
+        this.funcionarios.add(funcionario);
+    }
 
-public void addAgenda(Agenda agenda){
-    this.agendas.add(agenda);
-}
+    public void addAgenda(Agenda agenda){
+        this.agendas.add(agenda);
+    }
 
-public void addConsulta(Consulta consulta){
-    this.consultas.add(consulta);
-}
+    public void addConsulta(Consulta consulta){
+        this.consultas.add(consulta);
+    }
 
-// gets
+    public String getNome(){
+        return this.nome;
+    }
 
-public String getNome(){
-    return this.nome;
-}
+    public ArrayList<Animal> getPacientes() {
+        return this.pacientes;
+    }
 
-public ArrayList<Animal> getPacientes() {
-    return this.pacientes;
-}
+    public ArrayList<Funcionario> getFuncionarios(){
+        return this.funcionarios;
+    }
 
-public ArrayList<Funcionario> getFuncionarios(){
-    return this.funcionarios;
-}
+    public ArrayList<Agenda> getAgendas(){
+        return this.agendas;
+    }
 
-public ArrayList<Agenda> getAgendas(){
-    return this.agendas;
-}
-
-public ArrayList<Consulta> getConsultas(){
-    return this.consultas;
-}
+    public ArrayList<Consulta> getConsultas(){
+        return this.consultas;
+    }
 
 }
